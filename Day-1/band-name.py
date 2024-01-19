2f0f5cb5b9d9e85690395f64274592cc3fb4130c
Nawel Asgar <nawelasg@gmail.com>
2024-01-19 13:34:12 +0530

    Completed Day 1

---
+++ Day-1/band-name.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What is the Name of your City?\n")
+
+food = input("What is your favourite food?\n")
+
+print(f"Your Band Name is {city} {food}")
